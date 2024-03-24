99a62d4d2c36e32e19d5e0a979bc338dbb5f4e76
nitiphum <65030130@kmitl.ac.th>
2024-03-24 22:37:45 +0700

    commitex2

---
+++ Lab11_Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine(Calculator.AddValues(2, 3));
+System.Console.WriteLine(Calculator.AddValues(2.0f, 3.0f));
+System.Console.WriteLine(Calculator.AddValues(2L, 3L));
+
+public static class Calculator
+{
+    public static int AddValues(int a, int b)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int a, int b)");
+        return a + b;
+    }
+    public static long AddValues(int c, int d)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int c, int d)");
+        return c + d;
+    }
+    public static float AddValues(int f, int g)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int f, int g)");
+        return (long) (f + g);
+    }
+   public static long AddValues(long h, long i)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(long h, long i)");
+        return (long) (h + i);
+    }
+}
